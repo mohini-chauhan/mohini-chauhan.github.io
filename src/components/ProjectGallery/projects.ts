55b626bcca1ece17d5610f80803bf0bbafe01fde
mohini <chauhanmohini18@gmail.com>
2026-01-13 13:41:55 +0530

    gallery addition

--- src/components/ProjectGallery/projects.ts
+++ src/components/ProjectGallery/projects.ts
@@ -1,8 +1,12 @@
 import hikingSite from "../../assets/Hiking site.png";
+import unikaksha from "../../assets/Unikaksha.png";
+import reactWidgets from "../../assets/Widget.png";
+import smarterCode from "../../assets/Smarter code.png";
 import techDoc from "../../assets/tech doc.png";
 import virtualKitchen from "../../assets/virtual kitchen.png";
 import profileSample from "../../assets/profileSample.png";
 import productLanding from "../../assets/product landing.png";
+import frugal from "../../assets/Frugal analytics.png";
 
 export const ProjectData = [
   {
@@ -13,46 +17,50 @@ export const ProjectData = [
   },
   {
     id: 2,
-    title: "Hiking site",
-    link: "https://exploringhikes.netlify.app/"
+    title: "Unikaksha",
+    img: unikaksha,
+    link: "https://fir-unikaksha.web.app/"
   },
   {
     id: 3,
-    title: "Hiking site",
-    link: "https://exploringhikes.netlify.app/"
+    title: "React Widgets",
+    img: reactWidgets,
+    link: "https://github.com/mohini-chauhan/React-widgets?tab=readme-ov-file"
   },
   {
     id: 4,
-    title: "Hiking site",
-    link: "https://exploringhikes.netlify.app/"
+    title: "Smarter Code",
+    img: smarterCode,
+    link: "https://smartercode-d5499.web.app/"
   },
   {
     id: 5,
-    title: "Hiking site",
-    link: "https://exploringhikes.netlify.app/"
-  },
-  {
-    id: 6,
     title: "Technical Documentation",
     img: techDoc,
     link: "https://codepen.io/Mohinii/pen/NWpLjEO",
   },
   {
-    id: 7,
+    id: 6,
     title: "Virtual Kitchen",
     img: virtualKitchen,
     link: "https://codepen.io/Mohinii/pen/OJmEeVR",
   },
   {
-    id: 8,
+    id: 7,
     title: "Portfolio Website",
     img: profileSample,
     link: "https://codepen.io/Mohinii/pen/vYxzada",
   },
   {
-    id: 9,
+    id: 8,
     title: "Product Landing",
     img: productLanding,
     link: "https://codepen.io/Mohinii/full/VwpBLNQ",
+  },
+  {
+    id: 9,
+    title: "Frugal analytics",
+    img: frugal,
+    link: "https://codepen.io/Mohinii/full/NWpzgLb",  
   },
 ] as const;
